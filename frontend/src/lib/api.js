921971e71f170501ba4702b4e7b304c7a6d23963
Audranhiro <audran.lecene@gmail.com>
2026-05-03 22:59:13 +0200

    Use local backend fallback for frontend API

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-export const API = `${process.env.REACT_APP_BACKEND_URL}/api`;
+const backendUrl = (process.env.REACT_APP_BACKEND_URL || "http://localhost:8000").replace(/\/$/, "");
+export const API = `${backendUrl}/api`;
 
 export const api = axios.create({
   baseURL: API,
